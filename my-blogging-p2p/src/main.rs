475c9b60bf68308f4877f1280ec181294d1a1dd2
Owen Dewing <odewing@lion.lmu.edu>
2024-05-05 13:49:43 -0400

    final project

--- my-blogging-p2p/src/main.rs
+++ my-blogging-p2p/src/main.rs
@@ -11,16 +11,18 @@ use libp2p::{
     NetworkBehaviour, PeerId, Transport,
 };
 use log::{error, info};
+use std::io::{self, Write};
 use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
 use tokio::{fs, io::AsyncBufReadExt, sync::mpsc};
-use dialoguer::{Input, Password};
-
+use bcrypt::{hash, verify, DEFAULT_COST};
+use dialoguer::Input;
+use std::process;
 
 const STORAGE_FILE_PATH: &str = "./blog.json";
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+type Result<T, E = Box<dyn std::error::Error + Send + Sync>> = std::result::Result<T, E>;
 type Blogs = Vec<Blog>;
 
 static KEYS: Lazy<identity::Keypair> = Lazy::new(|| identity::Keypair::generate_ed25519());
@@ -35,6 +37,32 @@ struct Blog {
     body: String,
     public: bool,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    username: String,
+    password_hash: String,
+}
+
+impl User {
+    fn new(username: String, password: String) -> Result<Self, &'static str> {
+        let hashed_password = hash_password(password)?;
+        Ok(Self {
+            username,
+            password_hash: hashed_password,
+        })
+    }
+
+    fn verify_password(&self, password: &str) -> bool {
+        verify(password, &self.password_hash).unwrap_or(false)
+    }
+}
+
+fn hash_password(password: String) -> Result<String, &'static str> {
+    hash(password, DEFAULT_COST).map_err(|_| "Failed to hash password")
+}
+
+
 
 #[derive(Debug, Serialize, Deserialize)]
 enum ListMode {
@@ -164,12 +192,14 @@ async fn create_new_post(author: &str, subject: &str, body: &str) -> Result<()> 
 
 async fn publish_post(id: usize) -> Result<()> {
     let mut local_posts = read_local_posts().await?;
-    local_posts
-        .iter_mut()
-        .filter(|r| r.id == id)
-        .for_each(|r| r.public = true);
-    write_local_posts(&local_posts).await?;
-    Ok(())
+    if let Some(post) = local_posts.iter_mut().find(|r| r.id == id) {
+        post.public = true;
+        write_local_posts(&local_posts).await?;
+        info!("Published blogpost with id: {}", id);
+        Ok(())
+    } else {
+        Err("Blog post not found".into())
+    }
 }
 
 async fn read_local_posts() -> Result<Blogs> {
@@ -191,13 +221,26 @@ async fn main() {
     info!("Peer Id: {}", PEER_ID.clone());
     let (response_sender, mut response_rcv) = mpsc::unbounded_channel();
 
+    let _username = rpassword::prompt_password_stdout("Create your username: ").unwrap();
+
+    let password = rpassword::prompt_password_stdout("Create your password: ").unwrap();
+    
+    let user = match register_user() {
+        Ok(user) => user,
+        Err(err) => {
+            error!("Failed to register user: {}", err);
+            return;
+        }
+    };
+
+
     let auth_keys = Keypair::<X25519Spec>::new()
         .into_authentic(&KEYS)
         .expect("can create auth keys");
 
     let transp = TokioTcpConfig::new()
         .upgrade(upgrade::Version::V1)
-        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated()) // XX Handshake pattern, IX exists as well and IK - only XX currently provides interop with other libp2p impls
+        .authenticate(NoiseConfig::xx(auth_keys).into_authenticated())
         .multiplex(mplex::MplexConfig::new())
         .boxed();
 
@@ -227,6 +270,14 @@ async fn main() {
     )
     .expect("swarm can be started");
 
+    if login_user(&user, &password) {
+        println!("Welcome to Blogamania, {}!", user.username);
+        println!("Post your first blog using create post!");
+    } else {
+        println!("Login failed. Incorrect username or password.");
+        return;
+    }
+
     loop {
         let evt = {
             tokio::select! {
@@ -249,15 +300,19 @@ async fn main() {
                         .publish(TOPIC.clone(), json.as_bytes());
                 }
                 EventType::Input(line) => match line.as_str() {
-                    "ls p" => handle_list_peers(&mut swarm).await,
-                    cmd if cmd.starts_with("ls r") => handle_list_posts(cmd, &mut swarm).await,
-                    cmd if cmd.starts_with("create r") => handle_create_post_prompt().await,
-                    cmd if cmd.starts_with("publish r") => handle_publish_post(cmd).await,
+                    "discover peers" => handle_list_peers(&mut swarm).await,
+                    cmd if cmd.starts_with("see posts") => handle_list_posts(cmd, &mut swarm).await,
+                    cmd if cmd.starts_with("create post") => handle_create_post_prompt().await,
+                    cmd if cmd.starts_with("publish post") => handle_publish_post(cmd).await,
+                    cmd if cmd.starts_with("search posts") => handle_search_posts(&mut swarm).await,
+                    cmd if cmd.starts_with("delete post") => handle_delete_post_prompt().await,
+                    "logout" => handle_logout(&mut swarm).await,
                     _ => error!("unknown command"),
                 },
             }
         }
     }
+
 }
 
 async fn handle_list_peers(swarm: &mut Swarm<BlogBehavior>) {
@@ -271,7 +326,7 @@ async fn handle_list_peers(swarm: &mut Swarm<BlogBehavior>) {
 }
 
 async fn handle_list_posts(cmd: &str, swarm: &mut Swarm<BlogBehavior>) {
-    let rest = cmd.strip_prefix("ls r ");
+    let rest = cmd.strip_prefix("ls posts ");
     match rest {
         Some("all") => {
             let req = ListRequest {
@@ -296,30 +351,15 @@ async fn handle_list_posts(cmd: &str, swarm: &mut Swarm<BlogBehavior>) {
         None => {
             match read_local_posts().await {
                 Ok(v) => {
-                    info!("Local Posts ({})", v.len());
+                    info!("Previously Posted Blogs! ({})", v.len());
                     v.iter().for_each(|r| info!("{:?}", r));
                 }
-                Err(e) => error!("error fetching local blogposts: {}", e),
+                Err(e) => error!("error fetching previously posted blogposts: {}", e),
             };
         }
     };
 }
 
-// async fn handle_create_post(cmd: &str) {
-//     if let Some(rest) = cmd.strip_prefix("create r") {
-//         let elements: Vec<&str> = rest.split("|").collect();
-//         if elements.len() < 3 {
-//             info!("too few arguments - Format: author|subject|body");
-//         } else {
-//             let author = elements.get(0).expect("author is there");
-//             let subject = elements.get(1).expect("subject is there");
-//             let body = elements.get(2).expect("body is there");
-//             if let Err(e) = create_new_post(author, subject, body).await {
-//                 error!("error creating blogpost: {}", e);
-//             };
-//         }
-//     }
-// }
 async fn handle_create_post_prompt() {
     let author: String = Input::new().with_prompt("Enter author name").interact_text().unwrap();
     let subject: String = Input::new().with_prompt("Enter subject").interact_text().unwrap();
@@ -342,4 +382,71 @@ async fn handle_publish_post(cmd: &str) {
             Err(e) => error!("invalid id: {}, {}", rest.trim(), e),
         };
     }
+}
+async fn handle_search_posts(_swarm: &mut Swarm<BlogBehavior>) {
+    print!("Enter author's name to search");
+    io::stdout().flush().unwrap();
+    let author: String = Input::new()
+    .with_prompt("Enter author's name to search")
+    .interact_text()
+    .unwrap();
+
+    match read_local_posts().await {
+        Ok(posts) => {
+            let filtered_posts: Vec<&Blog> = posts.iter().filter(|post| post.author == author).collect();
+            if filtered_posts.is_empty() {
+                println!("No blogs found for author: {}", author);
+            } else {
+                println!("Blogs by author '{}':", author);
+                for post in filtered_posts {
+                    println!("{:?}", post);
+                }
+            }
+        },
+        Err(e) => error!("Error fetching previously posted blogposts: {}", e),
+    }
+}
+async fn delete_post_prompt() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let id_to_delete: usize = Input::<String>::new()
+        .with_prompt("Enter the ID of the post you want to delete:")
+        .interact_text()?
+        .parse()?;
+
+    let mut local_posts: Blogs = read_local_posts().await?;
+    
+    if let Some(index) = local_posts.iter().position(|post| post.id == id_to_delete) {
+        local_posts.remove(index);
+        write_local_posts(&local_posts).await?;
+        println!("Successfully deleted blog post with id: {}", id_to_delete);
+        Ok(())
+    } else {
+        println!("Blog post with id {} not found.", id_to_delete);
+        Ok(())
+    }
+}
+async fn handle_delete_post_prompt() {
+    if let Err(e) = delete_post_prompt().await {
+        error!("Error deleting post: {}", e);
+    }
+}
+
+
+fn register_user() -> Result<User, &'static str> {
+    let username = rpassword::prompt_password_stdout("Enter your username: ").unwrap();
+    let password = rpassword::prompt_password_stdout("Enter your password: ").unwrap();
+
+    User::new(username, password)
+}
+
+fn login_user(user: &User, password: &str) -> bool {
+    user.verify_password(password)
+}
+
+async fn logout(_swarm: &mut Swarm<BlogBehavior>) {
+    println!("Logged out successfully.");
+    process::exit(0);
+}
+
+async fn handle_logout(swarm: &mut Swarm<BlogBehavior>) {
+    logout(swarm).await;
 }
